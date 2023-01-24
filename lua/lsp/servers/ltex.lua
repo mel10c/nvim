c50dd737ecd4afb5674a7d597c5ab7fe67deda61
mel10c <mel.zhu@mail.utoronto.ca>
2023-01-23 21:47:59 -0500

    lsp: added ltex for markdown

--- lua/lsp/servers/ltex.lua
+++ lua/lsp/servers/ltex.lua
@@ -1,9 +1,16 @@
+local path = vim.fn.stdpath("config") .. "/spell/en.utf-8.add"
+local words = {}
+
+for word in io.open(path, "r"):lines() do
+    table.insert(words, word)
+end
+
 return {
     cmd = {"ltex-ls"};
-    filetypes = {'tex', 'bib', 'latex',};
+    filetypes = {'tex', 'bib', 'latex', "markdown"};
     settings = {
         ltex = {
-            enabled= {"latex", "tex", "bib"},
+            enabled= {"latex", "tex", "bib", "markdown"},
             checkFrequency="save",
             language="en-us",
             diagnosticSeverity="information",
@@ -13,6 +20,7 @@ return {
                 -- motherTongue= "pt-BR",
                 motherTongue= "en-us",
             };
-            },
+            dictionary = { ["en-us"] = words };
         };
-    }
+    };
+}
